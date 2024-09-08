51bd830c755956e1d6a4c4779db1a8f869c15dc7
Galigomez30 <ggomezgonzalez1@student.alamo.edu>
2024-09-08 18:40:28 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,42 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Pamela":return img`
+. . . . f f f . . . . f f f f . 
+. . . f d d d f . . e 1 1 1 f . 
+. . f d d 1 1 1 e e 1 1 1 1 1 f 
+. . f 1 1 e f 1 1 1 1 e f d 1 f 
+. . e 1 f e 1 1 1 1 1 d d e d e 
+. . . e e 1 1 1 1 1 1 1 d f e . 
+. . . . e d 9 1 1 1 1 9 1 e . . 
+f f . . f 1 1 1 3 3 1 1 1 f . . 
+f d f . f 3 1 1 1 1 1 1 d f . . 
+f d 1 f 1 1 3 3 3 3 3 3 f . . . 
+. f 1 1 1 1 d 1 1 1 1 1 f . . . 
+. . f f 1 1 d d 1 1 1 1 f . . . 
+. . . f 1 f f 1 f f f 1 f . . . 
+. . . f f f . f f . . f f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
